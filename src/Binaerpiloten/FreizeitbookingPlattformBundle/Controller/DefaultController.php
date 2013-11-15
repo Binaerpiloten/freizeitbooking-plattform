3ae5ee59573e96350cef115436dee7eee4a96ac9
Benedikt Budig <benedikt.budig@binaerpiloten.de>
2013-11-15 09:57:40 +0100

    Refactoring of provider lists

--- src/Binaerpiloten/FreizeitbookingPlattformBundle/Controller/DefaultController.php
+++ src/Binaerpiloten/FreizeitbookingPlattformBundle/Controller/DefaultController.php
@@ -8,45 +8,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
+    public function indexAction() {
         $regions = $this->getRegions();
         return $this->render('BinaerpilotenFreizeitbookingPlattformBundle:Default:index.html.twig',
                 array('regions' => $regions));
     }
 
     public function gokartListAction($regionURLName) {
-        // retrieve all gokart providers from database
-        $em = $this->getDoctrine()->getManager();
-        $region = $em->getRepository('Binaerpiloten\FreizeitbookingPlattformBundle\Entity\Region')
-                      ->findOneBy(array('urlname' => $regionURLName));
-
-        if ($region === null)
-            return new Response('Region unbekannt'); //TODO!
-
-        $category = $em->getRepository('Binaerpiloten\FreizeitbookingPlattformBundle\Entity\Category')
-                        ->findOneBy(array('name' => 'Gokart'));
-
-        $q = $em->createQuery("select t from Binaerpiloten\FreizeitbookingPlattformBundle\Entity\SEOText t where t.region = " . $region->getId() . " and t.category = " . $category->getId());
-        $result = $q->getResult();
-
-        $seotext = empty($result) ? null : $result[0];
-
-        $regions = $this->getRegions();
-
-        return $this->render('BinaerpilotenFreizeitbookingPlattformBundle:Default:gokartlist.html.twig', 
-                array('region' => $region, 'seotext' => $seotext, 'regions' => $regions));
+        return $this->renderGenericProviderList($regionURLName,'gokartlist.html.twig','Gokart');
     }
 
     public function gokartDetailsAction($regionURLName, $id) {
-        $em = $this->getDoctrine()->getManager();
-        $provider = $em->getRepository('Binaerpiloten\FreizeitbookingPlattformBundle\Entity\GokartProvider')
-                       ->findOneBy(array('id' => $id));
-
-        $regions = $this->getRegions();
-
-        return $this->render('BinaerpilotenFreizeitbookingPlattformBundle:Default:gokartdetails.html.twig', 
-                array('provider' =>$provider, 'regions' => $regions));
+        return $this->renderGenericProviderDetails($id,'gokartdetails.html.twig','GokartProvider');
     }
 
     // helper functions go here
@@ -55,5 +28,38 @@ class DefaultController extends Controller
         $em = $this->getDoctrine()->getManager();
         $regions = $em->getRepository('Binaerpiloten\FreizeitbookingPlattformBundle\Entity\Region')->findAll();
         return $regions;
+    }
+
+    protected function renderGenericProviderList($regionURLName, $templateName, $categoryName) {
+        $em = $this->getDoctrine()->getManager();
+        $region = $em->getRepository('Binaerpiloten\FreizeitbookingPlattformBundle\Entity\Region')
+                     ->findOneBy(array('urlname' => $regionURLName));
+
+        if ($region === null)
+            return new Response('Region unbekannt'); //TODO!
+
+        $category = $em->getRepository('Binaerpiloten\FreizeitbookingPlattformBundle\Entity\Category')
+                       ->findOneBy(array('name' => $categoryName));
+
+        $q = $em->createQuery("select t from Binaerpiloten\FreizeitbookingPlattformBundle\Entity\SEOText t where t.region = " . $region->getId() . " and t.category = " . $category->getId());
+        $result = $q->getResult();
+
+        $seotext = empty($result) ? null : $result[0];
+
+        $regions = $this->getRegions();
+
+        return $this->render('BinaerpilotenFreizeitbookingPlattformBundle:Default:' . $templateName,
+                array('region' => $region, 'seotext' => $seotext, 'regions' => $regions));
+    }
+
+    protected function renderGenericProviderDetails($id, $templateName, $providerEntityName) {
+        $em = $this->getDoctrine()->getManager();
+        $provider = $em->getRepository('Binaerpiloten\FreizeitbookingPlattformBundle\Entity\\' . $providerEntityName)
+                       ->findOneBy(array('id' => $id));
+
+        $regions = $this->getRegions();
+
+        return $this->render('BinaerpilotenFreizeitbookingPlattformBundle:Default:'. $templateName,
+                array('provider' =>$provider, 'regions' => $regions));
     }
 }
