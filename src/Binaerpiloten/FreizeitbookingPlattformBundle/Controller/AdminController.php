c21b264195ba43c2c9e7aa868a92c9b9a2ee126a
Nils Wisiol <info@nils-wisiol.de>
2013-11-21 19:21:29 -0500

    Added admin homepage

---
+++ src/Binaerpiloten/FreizeitbookingPlattformBundle/Controller/AdminController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Binaerpiloten\FreizeitbookingPlattformBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AdminController extends Controller
+{
+    public function indexAction() {
+        return $this->render('BinaerpilotenFreizeitbookingPlattformBundle:Admin:index.html.twig');
+    }
+}
